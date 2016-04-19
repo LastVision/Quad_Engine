bcd2758e1f959309a38ed452807397ca86289e0e
LastVision <daniel.carlsson92@gmail.com>
2016-04-19 16:41:29 +0200

    Added Empty Interface and Included SDL to InputManager

--- Solution/Input Manager/InputManager.cpp
+++ Solution/Input Manager/InputManager.cpp
@@ -33,3 +33,33 @@ InputManager::InputManager()
 InputManager::~InputManager()
 {
 }
+
+const bool InputManager::IsKeyDown(const unsigned short aKey) const
+{
+
+}
+
+const bool InputManager::IsKeyPressed(const unsigned short aKey) const
+{
+
+}
+
+const bool InputManager::IsKeyReleased(const unsigned short aKey) const
+{
+
+}
+
+const bool InputManager::IsMouseButtonDown(const unsigned short aKey) const
+{
+
+}
+
+const bool InputManager::IsMouseButtonPressed(const unsigned short aKey) const
+{
+
+}
+
+const bool InputManager::IsMouseButtonReleased(const unsigned short aKey) const
+{
+
+}
--- Solution/Input Manager/InputManager.h
+++ Solution/Input Manager/InputManager.h
@@ -5,6 +5,15 @@ public:
 	static void Create();
 	static void Destroy();
 	static InputManager* GetInstance();
+
+	const bool IsKeyDown(const unsigned short aKey) const;
+	const bool IsKeyPressed(const unsigned short aKey) const;
+	const bool IsKeyReleased(const unsigned short aKey) const;
+
+	const bool IsMouseButtonDown(const unsigned short aKey) const;
+	const bool IsMouseButtonPressed(const unsigned short aKey) const;
+	const bool IsMouseButtonReleased(const unsigned short aKey) const;
+
 private:
 	static InputManager* myInstance;
 	InputManager();
